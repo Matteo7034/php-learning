4543762de62fc230353e0929701d4463de6d7f73
Matteo7034 <matteo7034@gmail.com>
2026-09-09 19:28:11 +0200

    spinning cube

--- views/home.php
+++ views/home.php
@@ -1,2 +1,6 @@
 <h1>Benvenuto nel mio sito!</h1>
 <p>Questo è il contenuto principale della homepage caricato dinamicamente.</p>
+
+<div class="gif-container">
+    <img src="/assets/img/spinning-cube.gif" alt="Animazione cubo di benvenuto" width="300">
+</div>
